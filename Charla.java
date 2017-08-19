b49347790ae49d8d3ab5c44332a8e0683ece3de4
Andrea-Arguello <31134106+Andrea-Arguello@users.noreply.github.com>
2017-08-18 20:50:14 -0600

    Update Charla.java

--- Charla.java
+++ Charla.java
@@ -1,7 +1,5 @@
 /** 
 @author Ana Lucia Hernandez. 17138.
-Cindy Antillon. 14581.
-Andrea Arguello. 17801.
 @proposito  modela la informacion de una charla DELVA
 @since 04/08/2017
 Charla.java
@@ -17,20 +15,33 @@ public class Charla
 	private String fecha;
 	private ArrayList<Estudiante> asistentes;
 
-	public Charla(String nombre, String salon, String hora, String fecha){
+	/**Guarda toda la informacion de una nueva charla
+     * @param nombre Nombre de la charla
+	 * @param salon Lugar de la charla
+	 * @param hora Hora de la charla
+	 * @param fecha Fecha de la charla
+     */
+	public Charla(String nombre, String salon, String hora, String fecha)
+	{
 		this.nombre = nombre;
 		this.lugar = salon;
 		this.hora = hora;
 		this.fecha = fecha;
-		
 	}
 	
-	public String getNombre(){
+	/**
+	 * @return nombre
+     */
+	public String getNombre()
+	{
 		return nombre;
 	}
 	
-	public ArrayList<Estudiante> getAsistentes(){
+	/**
+	 * @return asistentes
+     */
+	public ArrayList<Estudiante> getAsistentes()
+	{
 		return asistentes;
 	}
-	
 }
