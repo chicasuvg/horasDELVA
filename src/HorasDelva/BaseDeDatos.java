5c8fe885ea592183e26dc6ff876b3e090f2c6dbc
Ana <Ana@192.168.1.12>
2017-11-11 00:11:47 -0600

    sabado 12:11am

--- src/HorasDelva/BaseDeDatos.java
+++ src/HorasDelva/BaseDeDatos.java
@@ -17,6 +17,14 @@ public class BaseDeDatos {
     private Datastore ds;
     private List<Administrador> administradores;
     private List<Estudiante> estudiantes;
+    private List<Charla> charlas;
+    private Query<Administrador> query;
+    private Query<Estudiante> query1;
+    private Query<Charla> query2;
+    private boolean admin;
+    private boolean est;
+    public Estudiante usuarioE;
+    public Administrador usuarioA;
     
     public BaseDeDatos()
     {
@@ -24,30 +32,105 @@ public class BaseDeDatos {
         Morphia morphia = new Morphia();
         morphia.map(Administrador.class).map(Estudiante.class).map(Charla.class).map(Registro.class);
         ds = morphia.createDatastore(mongo, "HorasDelva");
-    }
-    public void crearUsuario(String tipoUsuario, String nombreUsuario, String contrasena)
-    {
-        Query<Administrador> query = ds.createQuery(Administrador.class);
+        query = ds.createQuery(Administrador.class);
         List<Administrador> busqueda = query.asList();
         administradores = busqueda;
-        
+        query1 = ds.createQuery(Estudiante.class);
+        List<Estudiante> busqueda1 = query1.asList();
+        estudiantes = busqueda1;
+        List<Charla> busqueda2 = query2.asList();
+        charlas = busqueda2;
+    }
+    public boolean getEst()
+    {
+        return est;
+    }
+    public boolean getAdmin()
+    {
+        return admin;
+    }
+    public Estudiante getEstudiante()
+    {
+        return usuarioE;
+    }
+    public Administrador getAdministrador()
+    {
+        return usuarioA;
+    }
+    public boolean crearUsuario(String tipoUsuario, String nombreUsuario, String contrasena)
+    {
+        boolean existeUsuario = false;
+        for (Administrador admin : administradores)
+        {
+            if (admin.getNombre().equals(nombreUsuario))
+            {
+                existeUsuario = true;
+            }
+        }
+        for (Estudiante est :estudiantes)
+        {
+            if (est.getNombre().equals(nombreUsuario))
+            {
+                existeUsuario = true;
+            }
+        }
         if(tipoUsuario.equals("Administrador"))
         {
             Administrador nuevo = new Administrador(nombreUsuario.toUpperCase(), contrasena);
-            //nuevo.setCharlas(); //para que el nuevo administrador tenga acceso a todas las charlas previamente ingresadas
-            administradores.add(nuevo);
+            if (!administradores.isEmpty())
+            {
+                ArrayList<Charla> charlas = administradores.get(0).mostrarCharlas();
+                nuevo.setCharlas(charlas);
+            } //para que el nuevo administrador tenga acceso a todas las charlas previamente ingresadas
+            ds.save(nuevo);
             
         }
         if (tipoUsuario.equals("Estudiante"))
         {
             Estudiante nuevo = new Estudiante(nombreUsuario.toUpperCase(), contrasena);
-            estudiantes.add(nuevo);
-            for(Administrador admin : administradores)
-            {
-                admin.getAlumnos().add(nuevo);//para que todos los administradores tengan la misma lista de estudiantes.
-            }
-            
+            ds.save(nuevo);
         }
+        return existeUsuario;
+    }
+    public boolean verificacionIngreso(String nombreUsuario, String contrasena)
+    {
+        boolean puedeIngresar = false;
+        for(Estudiante estudiante: estudiantes)
+        {
+            if(estudiante.getNombre().equals(nombreUsuario) && estudiante.getContrasena().equals(contrasena))
+            {
+                puedeIngresar = true;
+                est = true;
+                usuarioE = estudiante;
+            }
+        }
+        for(Administrador administrador: administradores)
+        {
+            if(administrador.getNombre().equals(nombreUsuario) && administrador.getContrasena().equals(contrasena))
+            {
+                puedeIngresar = true;
+                admin = true;
+                usuarioA = administrador;
+            }
+        }
+        return puedeIngresar;
+    }
+    public void ingresarCharla(String nombre, String salon, String time, String fecha, int duracion)
+    {
+        boolean existeCharla = false;
+        for (Charla delva : charlas)
+        {
+            if (delva.getNombre().equals(nombre))
+            {
+                existeCharla = true;
+            }
+        }
+        if (existeCharla == true)
+        {
+            Charla delva = new Charla(nombre, salon, time, fecha, duracion);
+            usuarioA.agregarCharla(delva);
+        }
+        
     }
     
 }
